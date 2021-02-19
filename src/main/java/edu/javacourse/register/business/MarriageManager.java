5f6c65ae5b45ec53305a77c74582cdd992763b32
sergei1733 <sergei1733@yandex.ru>
2021-02-19 23:41:10 +0500

    marriage

---
+++ src/main/java/edu/javacourse/register/business/MarriageManager.java
@@ -0,0 +1,12 @@
+package edu.javacourse.register.business;
+
+import edu.javacourse.register.view.MarriageRequest;
+import edu.javacourse.register.view.MarriageResponse;
+
+public class MarriageManager {
+
+    public MarriageResponse findMarriageCertificate(MarriageRequest request){
+
+        throw new UnsupportedOperationException("Unsupported");
+    }
+}
