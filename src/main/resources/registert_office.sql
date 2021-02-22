38f9e691b900c3d4116bd54d4e74bac4d44629b7
sergei1733 <sergei1733@yandex.ru>
2021-02-22 18:07:24 +0500

    domain model

--- src/main/resources/registert_office.sql
+++ src/main/resources/registert_office.sql
@@ -25,3 +25,6 @@ CREATE TABLE ro_passport(
 INSERT INTO ro_person (sex, first_name, last_name, patronymic, date_birth)
 VALUES(1,'Елена', 'Васильева','Сергеевна','1998-03-24'),
       (2,'Олег', 'Васильев','Петрович','1997-10-16');
+
+INSERT INTO ro_passport (person_id, seria, number, date_issue, issue_department)
+VALUES (1, '40000', '123456', '2018-04-10', 'Department Passport');
