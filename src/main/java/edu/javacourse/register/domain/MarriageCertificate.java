03b902a194bfacb0acb5efde602a60413630f32e
sergei1733 <sergei1733@yandex.ru>
2021-02-22 18:35:08 +0500

    domain model

--- src/main/java/edu/javacourse/register/domain/MarriageCertificate.java
+++ src/main/java/edu/javacourse/register/domain/MarriageCertificate.java
@@ -1,15 +1,28 @@
 package edu.javacourse.register.domain;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Table(name = "ro_marriage_certificate")
+@Entity
 public class MarriageCertificate {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "marriage_certificate_id")
     private Long MarriageCertificateId;
+    @Column(name = "marriage_certificate")
     private String number;
+    @Column(name = "date_issue")
     private LocalDate issueDate;
+    @ManyToOne(cascade = {CascadeType.REFRESH}, fetch = FetchType.LAZY)
+    @Column(name = "father_id")
     private PersonMale husband;
+    @ManyToOne(cascade = {CascadeType.REFRESH}, fetch = FetchType.LAZY)
+    @Column(name = "mother_id")
     private PersonFemale wife;
+    @Column(name = "active")
     private boolean active;
+    @Column(name = "end_date")
     private LocalDate endDate;
 
     public Long getMarriageCertificateId() {
--- src/main/resources/registert_office.sql
+++ src/main/resources/registert_office.sql
@@ -22,6 +22,21 @@ CREATE TABLE ro_passport(
     FOREIGN KEY (person_id) REFERENCES ro_person(person_id) ON DELETE RESTRICT
 );
 
+CREATE TABLE ro_birth_certificate (
+    birth_certificate_id SERIAL,
+    number_certificate varchar(20) not null,
+    date_issue date not null,
+    person_id integer not null,
+    father_id integer,
+    mother_id integer,
+    PRIMARY KEY (birth_certificate_id),
+    FOREIGN KEY (person_id) REFERENCES ro_person(person_id) ON DELETE RESTRICT
+    FOREIGN KEY (father_id) REFERENCES ro_person(person_id) ON DELETE RESTRICT
+    FOREIGN KEY (mother_id) REFERENCES ro_person(person_id) ON DELETE RESTRICT
+);
+
+
+
 INSERT INTO ro_person (sex, first_name, last_name, patronymic, date_birth)
 VALUES(1,'Елена', 'Васильева','Сергеевна','1998-03-24'),
       (2,'Олег', 'Васильев','Петрович','1997-10-16');
